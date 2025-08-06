c5b2270453fa3c1431dc2ebb11996753b2a75c3d
Gibrail Zaidi <gibrailzaidi@gmail.com>
2025-08-06 21:01:05 +0530

    add server base url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/v1', // your url
+  baseURL: 'https://smartleads-ai-crm.onrender.com', //'http://localhost:5000/api/v1'
   headers: { 'Content-Type': 'application/json' },
 });
 
